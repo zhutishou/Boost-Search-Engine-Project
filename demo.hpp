2c1756c95dddc0a5b43cdcaa3af4211090543a51
jiaofi <166228543+zhutishou@users.noreply.github.com>
2025-07-14 21:09:12 +0800

    Create demo.hpp

---
+++ demo.hpp
@@ -0,0 +1,32 @@
+#include "inc/cppjieba/Jieba.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+const char* const DICT_PATH = "./dict/jieba.dict.utf8";
+const char* const HMM_PATH = "./dict/hmm_model.utf8";
+const char* const USER_DICT_PATH = "./dict/user.dict.utf8";
+const char* const IDF_PATH = "./dict/idf.utf8";
+const char* const STOP_WORD_PATH = "./dict/stop_words.utf8";
+
+
+
+int main(int argc, char** argv) 
+{
+
+    cppjieba::Jieba jieba(DICT_PATH,
+    HMM_PATH,
+    USER_DICT_PATH,
+    IDF_PATH,
+    STOP_WORD_PATH);
+    vector<string> words;
+    string s;
+    s = "⼩明硕⼠毕业于中国科学院计算所，后在⽇本京都⼤学深造";
+    cout << s << endl;
+    cout << "[demo] CutForSearch" << endl;
+    jieba.CutForSearch(s, words);
+    cout << limonp::Join(words.begin(), words.end(), "/") << endl;
+    return EXIT_SUCCESS;
+
+}
