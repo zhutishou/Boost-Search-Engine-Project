d2be37aca757fc8c0c89a0cad7d97964cc722d75
jiaofi <166228543+zhutishou@users.noreply.github.com>
2025-07-15 20:59:23 +0800

    Update index.hpp

--- index.hpp
+++ index.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <ifstream>
+#include <unordered_map>
 #include "util.hpp"
 
 
@@ -33,11 +34,9 @@ namespace cx_index
     public:
         Index()
         {
-
         }
         ~Index()
         {
-
         }
 
         //正排:根据doc_id查找文档内容
@@ -98,6 +97,7 @@ namespace cx_index
         }
 
     private:
+        //正排：
         DocInfo* BuildForwardIndex(const std::string& line)
         {
             //1.解析line，将字符串切分
@@ -123,8 +123,53 @@ namespace cx_index
             
             return &forward_index.back();
         }
+        //倒排：
         bool BuildInvertedIndex(const DocInfo& doc)
         {
+            struct word_cnt
+            {
+                word_cnt():
+                    title_cnt(0),content_cnt(0)
+                {}
+                int title_cnt;
+                int content_cnt;
+            };
+            //1.分词：
+            //对标题分词
+            std::vector<std::string> title_words;
+            ns_util::JiebaUtil::CutString(doc.title,&title_words);
+            //对内容分词
+            std::vector<std::string> content_words;
+            ns_util::JiebaUtil::CutString(doc.content,&content_words);
+
+            //2.词频统计：注意：不区分大小写，用boost::to_lower()
+            std::unordered_map<std::string,word_cnt> word_map;
+            //对标题进行统计：
+            for(std::string e:title_words)
+            {
+                boost::to_lower(e);
+                word_map[e].title_cnt++;
+            }   
+            //对内容进行统计：
+            for(std::string e:content_words)
+            {
+                boost::to_lower(e);
+                word_map[e].content_cnt++;
+            }
+
+            //3.相关性处理
+            //在这里我们直接将title权重设置为10，content权重设置为1
+#define X 10
+#define Y 1
+            for(auto& word_pair:word_map)//处理成pair类型
+            {
+                InvertedELem item;
+                item.doc_id=doc.doc_id;//DocInfo
+                item.word=word_pair.first;
+                item.weight=word_pair.second.title_cnt*X+word_pair.second.content_cnt*Y;
+                InvertedList& Inverted_list=inverted_index[word_pair.first];
+                Inverted_list.push_back(std::move(item));
+            }
             return true;
         }
 
@@ -133,6 +178,6 @@ namespace cx_index
         std::vector<DocInfo> forward_index;
         //当时倒排时，通过关键字找对应的ID等信息(可能存在一对多的关系),此时利用unordered_map来解决(hash),注意second可能存在多个，要利用vector存放
 typedef std::vector<InvertedELem> InvertedList;      
-        std::unordered_map<std::string,InvertedList> inverted_index;
+        std::unordered_map<std::string,InvertedList> inverted_index;//first--存放词,second存放一条链表，表中是词为first的不同权重/doc_id的节点
     };
 }
