c032a97dedf44511bad7417791457a8e1974bbb9
jiaofi <166228543+zhutishou@users.noreply.github.com>
2025-07-17 20:50:02 +0800

    Update searcher.hpp

--- searcher.hpp
+++ searcher.hpp
@@ -1,7 +1,10 @@
+#pragma once
+
 #include <mutex>
 #include <algorithm>
 #include <jsoncpp/json/json.h>
 #include "index.hpp"
+#include "util.hpp"
 
 
 
@@ -15,13 +18,16 @@ namespace ns_searcher
         ~Searcher()
         {}
         //初始化搜索引擎
-        void InitSearcher(const std::string& output)
+        void InitSearcher(const std::string& input)
         {
             //1.获取或创建Index对象
             index=ns_index::Index::GetInstance();//得到单例的唯一对象
-
+            //查看;
+            std::cout<<"获取单例成功"<<std::endl;
             //2.根据对象建立索引
-            index->BuildIndex(output);//bool BuildIndex(const string& input)//input即/data/raw_html/raw.txt文件
+            index->BuildIndex(input);//bool BuildIndex(const string& input)//input即/data/raw_html/raw.txt文件
+            std::cout<<"构建正排和倒排索引成功"<<std::endl;
+        
         }
         //根据用户传输的关键词来建立相应的索引，并返回给用户浏览器Json对象
         void Search(const std::string& query,std::string* json_string)
@@ -72,14 +78,42 @@ namespace ns_searcher
                 }
                 Json::Value elem;
                 elem["title"]=doc->title;
-                elem["desc"]=doc->content;//注意我们想要的是content的一部分内容
+                elem["desc"]=GetDesc(doc->content,item.word);//注意我们想要的是content的一部分内容
                 elem["url"]=doc->url;
                 root.append(elem);
             }            
             Json::StyledWriter writer;
             *json_string = writer.write(root);
         }
+        //截取内容：部分content
+        std::string GetDesc(const std::string& html_content,const std::string& word)
+        {
+            //我们这里假设要求截取word出现的前五十个字节和后100个字节，不够就截取到开头和结尾
+            //1.在content中查找word首次出现的位置
+            std::size_t pos=html_content.find(word);
+            //检查：
+            if(pos==std::string::npos)
+            {
+                return "None";
+            }
 
+            //2.确定截取范围
+            //先假设截取范围
+            std::size_t start=0;
+            std::size_t end=html_content.size()-1;
+            std::size_t front=50;
+            std::size_t back=100;
+            if(pos-front > start)
+                start=pos-front;
+            if(pos+back<end)
+                end=pos+back;
+
+            //3.截取相应的字符串
+            //先检查start与end位置是否正确
+            if(start>=end)
+                return "None";
+            return html_content.substr(start,end-start);
+        }
 
     private:
         ns_index::Index* index;//供系统调用的索引
