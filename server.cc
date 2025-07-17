e37079a94b0e57095e801779126e2f069ccac282
jiaofi <166228543+zhutishou@users.noreply.github.com>
2025-07-17 20:49:40 +0800

    Update server.cc

--- server.cc
+++ server.cc
@@ -6,19 +6,19 @@ const std::string input="data/raw_html/raw.txt";
 int main()
 {
     //定义一个搜索对象
-    ns_searcher::Searcher* search=new ns_searcher::Searcher();
-    search->InitSearcher(input);
+    //ns_searcher::Searcher* search=new ns_searcher::Searcher();
+    ns_searcher::Searcher search;
+    search.InitSearcher(input);
     std::string query;//用户输入关键词
     std::string json_string;//输出给用户的json串
     while(true)//服务一直启动
     {
         std::cout<<"请搜索输入："<<std::endl;
         std::cin>>query;
-        search->Search(query,&json_string);
+        search.Search(query,&json_string);
         std::cout<<json_string<<std::endl;
     }
 
 
     return 0;
 }
-
