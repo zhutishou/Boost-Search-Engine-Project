c35081e675cb269dfe689b7b02f1778cb4950afd
jiaofi <166228543+zhutishou@users.noreply.github.com>
2025-07-18 15:44:33 +0800

    Update and rename server.cc to debug.cc

--- debug.cc
+++ debug.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "searcher.hpp"
 #include <string>
+#include <cstdio>
+
+
 
 const std::string input="data/raw_html/raw.txt";
 int main()
@@ -11,10 +14,14 @@ int main()
     search.InitSearcher(input);
     std::string query;//用户输入关键词
     std::string json_string;//输出给用户的json串
+    char buff[1024];
     while(true)//服务一直启动
     {
-        std::cout<<"请搜索输入："<<std::endl;
-        std::cin>>query;
+        std::cout<<"请搜索输入你的搜索语句："<<std::endl;
+        fgets(buff,strlen(buff)-1,stdin);
+        buff[strlen(buff)-1]='\0';
+        query=buff;
+        //std::cin>>query;
         search.Search(query,&json_string);
         std::cout<<json_string<<std::endl;
     }
